e13759de967ac65712ce588d2cb8438229442b3c
bacf5 <brunoagustincarusofassa@gmail.com>
2025-09-16 23:45:49 +0200

    Initial commit

---
+++ dolaruco-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dolaruco-app
+//
+//  Created by Bruno Agustin Caruso Fassa on 16.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dolaruco-app/dolaruco_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dolaruco_appApp.swift
+//  dolaruco-app
+//
+//  Created by Bruno Agustin Caruso Fassa on 16.09.25.
+//
+
+import SwiftUI
+
+@main
+struct dolaruco_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
