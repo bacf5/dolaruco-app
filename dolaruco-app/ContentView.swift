6164ba7bca128461aed3f7ea6ab0cdee3624fb63
bacf5 <brunoagustincarusofassa@gmail.com>
2025-09-19 12:39:26 +0200

    Added ProgressView when is fetching / Some minor styling

--- dolaruco-app/ContentView.swift
+++ dolaruco-app/ContentView.swift
@@ -10,38 +10,61 @@ struct ContentView: View {
     }
     
     var body: some View {
-        HStack() {
-            Image("mascota")
-                .resizable()
-                .frame(width: 150, height: 150)
+        HStack{
+            VStack {
+                Image("mascota")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .shadow(color: .black, radius: 4)
                 
-            
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.2.circlepath.circle")
+                        .font(.title2)
+                        .foregroundStyle(.green)
+                }
+                .buttonStyle(.borderless)
+            }
+                
             VStack{
                 Text("Dolar hoy \(currentDate, style: .date)")
                     .font(.title2)
                     .padding(.top)
                 
-                List(vm.dollars, id: \.nombre) { dolar in
-                    HStack(alignment: .center){
-                        VStack(alignment: .leading) {
-                            Image("\(dolar.moneda)")
-                                .resizable()
-                                .frame(width: 24, height: 24)
+                VStack {
+                    if vm.isLoading {
+                        ProgressView("Cargando...")
+                    } else {
+                        List(vm.dollars, id: \.nombre) { dolar in
+                            HStack(alignment: .center){
+                                VStack(alignment: .leading) {
+                                    Image("\(dolar.moneda)")
+                                        .resizable()
+                                        .frame(width: 24, height: 24)
+                                    
+                                    Text("\(dolar.nombre)")
+                                        .opacity(0.4)
+                                    //                            Divider()
+                                }
                                 
-                            Text("\(dolar.nombre)")
-                                .opacity(0.4)
-//                            Divider()
-                        }
-                        
-                        Spacer()
-                        Text(dolar.venta.formatAsCurrency())
+                                Spacer()
+                                Text(dolar.venta, format: .currency(code: "USD"))
+                            }
+                        }.scrollContentBackground(.hidden)
                     }
                 }.task {
-                    await vm.populateDollars()
+//                    await vm.populateDollars()
                 }
+            
             }
+        }.frame(width: 500, height: 450)
+        HStack{
+            Text("Copyright © 2025 - Bruno")
+                                .font(.system(size: 8))
+                                .foregroundColor(Color.gray)
+                                .padding(.bottom)
         }
-        .frame(width: 450, height: 450)
     }
 }
     
